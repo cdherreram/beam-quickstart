14e02b057046d4ac866186f792c7818dff6aaf9b
Cristian David Herrera Medina <cdherreram@unal.edu.co>
2023-03-24 23:39:28 -0500

    Se carga una primera versión de main.py

--- main.py
+++ main.py
@@ -11,3 +11,36 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+
+import apache_beam as beam
+import argparse
+
+from apache_beam.options.pipeline_options import PipelineOptions
+
+def main():
+    parser = argparse.ArgumentParser(description="Nuestro primer pipeline")
+    parser.add_argument("--entrada", help="Fichero de entrada")
+    parser.add_argument("--salida", help="Fichero de salida")
+
+    our_args, beam_args = parser.parse_known_args()
+
+    run_pipeline(our_args, beam_args)
+
+def run_pipeline(custom_args, beam_args):
+    entrada = custom_args.entrada
+    salida = custom_args.salida
+
+    opts = PipelineOptions(beam_args)
+
+    with beam.Pipeline(options = opts) as p:
+        # Extraer cada línea 
+        lineas: PCollection[str] = p | beam.io.ReadFromText(entrada)
+        palabras  = lineas | beam.FlatMap(lambda l:l.split())
+        contadas: PCollection[Tuple[str,int]] = palabras | beam.combiners.Count.PerElement()
+        palabras_top: PCollection[Tuple[str,int]] = contadas | beam.combiners.Top.Of(5, key=lambda kv:kv[1])
+        palabras_top | beam.Map(print)
+
+
+
+if __name__ == "__main__":
+    main()
