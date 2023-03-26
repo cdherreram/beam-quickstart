be19992372c29c183c1f1d95459c328691953419
Cristian David Herrera Medina <cdherreram@unal.edu.co>
2023-03-26 10:43:38 -0500

    Se adicionan cambios para limpiar las palabras

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ import argparse
 
 from apache_beam.options.pipeline_options import PipelineOptions
 
+def sanitizar_palabra(palabra):
+    para_quitar = [',', '.', '-', ':', ' ', "'", '"']
+
+    for simbolo in para_quitar:
+        palabra = palabra.replace(simbolo, '')
+
+    palabra = palabra.lower()
+    palabra = palabra.replace("á", "a")
+    palabra = palabra.replace("é", "e")
+    palabra = palabra.replace("í", "i")
+    palabra = palabra.replace("ó", "o")
+    palabra = palabra.replace("ú", "u")
+
+    return palabra
+
 def main():
     parser = argparse.ArgumentParser(description="Nuestro primer pipeline")
     parser.add_argument("--entrada", help="Fichero de entrada")
@@ -30,15 +45,15 @@ def main():
 def run_pipeline(custom_args, beam_args):
     entrada = custom_args.entrada
     salida = custom_args.salida
-    n_palabras = custom_args.n_palabras
-    print(n_palabras)
+    n_palabras = custom_args.n_palabras    
     opts = PipelineOptions(beam_args)
 
     with beam.Pipeline(options = opts) as p:
         # Extraer cada línea 
         lineas: PCollection[str] = p | beam.io.ReadFromText(entrada)
         palabras  = lineas | beam.FlatMap(lambda l:l.split())
-        contadas: PCollection[Tuple[str,int]] = palabras | beam.combiners.Count.PerElement()
+        limpiadas = palabras | beam.Map(sanitizar_palabra)
+        contadas: PCollection[Tuple[str,int]] = limpiadas | beam.combiners.Count.PerElement()
         palabras_top_lista: PCollection[Tuple[str,int]] = contadas | beam.combiners.Top.Of(n_palabras, key=lambda kv:kv[1])
         palabras_top = palabras_top_lista | beam.FlatMap(lambda x: x)
         formateado: PCollection[str] = palabras_top | beam.Map(lambda kv: '%s,%d' % (kv[0],kv[1]))
